8cf5e079100330d9961b7980b76241c07fc832db
Nandani Paliwal <paliwalnandani12@gmail.com>
2023-08-09 16:46:45 +0530

    dark theme applied

--- components/header.tsx
+++ components/header.tsx
@@ -1,73 +1,109 @@
-"use client"
-import Image from 'next/image'
-import { useState } from 'react'
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
+export function Header() {
+  const [screenModeState, setScreenModeState] = useState<"dark" | "light">(
+    "light"
+  );
 
- export function Header(){
+  const [ inputData, setInputData ] = useState<string>("");
 
-    const [screenModeState, setScreenModeState] = useState<"dark" | "light">("light")
+  const bg = {
+    darkDesktopBg: "/bg-desktop-dark.jpg",
+    darkMobileBg: "/bg-mobile-dark.jpg",
+    lightMobileBg: "/bg-mobile-light.jpg",
+    lightDeskBg: "/bg-desktop-light.jpg",
+  };
 
-    const [ screenType, setScreenType ] = useState<"desktop" | "mobile">("mobile")
-
-    const bg = 
-        {
-            darkDesktopBg: '/bg-desktop-dark.jpg',
-            darkMobileBg: '/bg-mobile-dark.jpg',
-            lightMobileBg: '/bg-mobile-light.jpg',
-            lightDeskBg: '/bg-desktop-light.jpg'
-        }
-    
-
-    const setBgFunction = () => {
-        if (screenType === "mobile"){
-            if (screenModeState === "light"){
-                return bg.lightMobileBg;
-            }
-            else(screenModeState === "dark")
-            {
-                return bg.darkMobileBg;
-            }    
-                    
-        } 
-        else (screenType === "desktop")
-        {
-            if (screenModeState === "light")
-            {
-                return bg.lightDeskBg
-            }    
-        
-            else(screenModeState === "dark")
-            {
-                return bg.darkDesktopBg
-            }
-            
-        }
+  const setMobileBgFunction = () => {
+    if (screenModeState === "light") {
+      return bg.lightMobileBg;
+    } else screenModeState === "dark";
+    {
+      return bg.darkMobileBg;
     }
+  };
 
-    const toggleScreenMode = () => {
-
-
+  const setDesktopBgFunction = () => {
+    if (screenModeState === "light") {
+      return bg.lightDeskBg;
+    } else screenModeState === "dark";
+    {
+      return bg.darkDesktopBg;
     }
+  };
 
+  const toggleScreenMode = () => {
+    screenModeState === "light"
+      ? setScreenModeState("dark")
+      : setScreenModeState("light");
+  };
 
-    return(
-        <div className="container flex flex-col">
-            <div className='absolute'>
-                <Image src={`${setBgFunction}`} alt='' width={12000} height={1200} />
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(event.target.value)
+  }
+
+  return (
+    <div
+      className={`container relative flex flex-col w-screen h-screen ${
+        screenModeState === "dark" ? "bg-dark" : "bg-white"
+      }`}
+    >
+      <div className="header">
+        {/* Mobile Image */}
+        <Image
+          src={setMobileBgFunction()}
+          alt=""
+          width={12000}
+          height={1200}
+          className="block md:hidden"
+        />
+        {/* Desktop Image */}
+        <Image
+          src={setDesktopBgFunction()}
+          alt=""
+          width={12000}
+          height={1200}
+          className="hidden md:block"
+        />
+      </div>
+      <div className="todo-tab flex flex-col absolute justify-center items-center p-3 w-full">
+        <div className="flex flex-col  justify-center items-center p-3 w-11/12 gap-10 md:gap-4 md:w-1/2">
+          <div className="title flex justify-between items-center w-full">
+            <h1 className="font-bold text-white text-3xl tracking-widest">
+              TODO
+            </h1>
+            <Image
+              src={`${
+                screenModeState === "light" ? "/icon-moon.svg" : "icon-sun.svg"
+              }`}
+              alt=""
+              width={26}
+              height={26}
+              onClick={toggleScreenMode}
+            />
+          </div>
+          <div className={`flex justify-start space-x-4 items-center py-3 px-3 rounded w-full font-normal border-verylightgray ${
+                screenModeState === "dark" ? "bg-lightdark text-lightgrayishblue " : "bg-white text-darkgrayishblue"
+              }`}>
+            <div className="flex justify-center items-center rounded-full border w-5 h-5">
+                <Image src='/icon-check.svg' alt="check-icon" width={11} height={9} className="hidden"/>
             </div>
-            <div className='flex flex-col justify-between items-center relative m-3 p-3 mt-8 '>
-                <div className='title flex justify-between items-center w-full'>
-                    <h1 className='font-bold text-white text-3xl tracking-widest'>TODO</h1>
-                    <Image src={'/icon-moon.svg'} alt='' width={26} height={26}  />
-
-                </div>
-                <div className='new-to-do'></div>
-                <div className='list-of-todo'>
-
-                </div>
-
-            </div>
-
+            <input
+              name="todo-list"
+              type="text"
+              placeholder="Create a new todo..."
+              value={inputData}
+              onChange={handleChange}
+              className={`focus:ring-none focus:border-none ${
+                screenModeState === "dark" ? "bg-lightdark text-lightgrayishblue " : "bg-white text-darkgrayishblue"
+              }`}
+            />
+          </div>
+          <div className="list-of-todo"></div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        dark: 'hsl(235, 21%, 11%)',
+        lightdark: 'hsl(235, 24%, 19%)',
+        darkgrayishblue: 'hsl(235, 19%, 35%)',
+        lightgrayishblue: 'hsl(234, 39%, 85%)',
+        verylightgray: 'hsl(0, 0%, 98%)',
+      },
       letterSpacing: {
         widest: '0.5rem',
       }
